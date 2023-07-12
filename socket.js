48262eb329be12d43a7024e621d5c98d5ef2969e
QasimJaffry <qhassan1213@gmail.com>
2023-07-13 04:19:32 +0500

    socket fixes

--- socket.js
+++ socket.js
@@ -7,83 +7,39 @@ module.exports.initIO = (httpServer) => {
   });
 
   IO.use((socket, next) => {
-    const callerId = socket.handshake.query.callerId;
-    socket.user = callerId;
+    const query = socket.handshake.query;
+    socket.user = query;
     next();
   });
 
-  // IO.on("connection", (socket) => {
-  //   console.log(socket.user, "Connected");
-  //   socket.join(socket.user);
-
-  //   socket.on("offer", (offer, recipientId) => {
-  //     console.log(recipientId, "recipientIdrecipientId");
-
-  //     socket.to(recipientId).emit("offer", offer, socket.user);
-  //   });
-
-  //   socket.on("answer", (answer, senderId) => {
-  //     console.log(senderId, "senderId");
-  //     socket.to(senderId).emit("answer", answer);
-  //   });
-
-  //   socket.on("iceCandidate", (candidate, recipientId) => {
-  //     console.log(recipientId, "ice CANDI");
-
-  //     socket.to(recipientId).emit("iceCandidate", candidate, socket.user);
-  //   });
-
-  //   socket.on("endCall", (recipientId) => {
-  //     socket.to(recipientId).emit("endCall", socket.user);
-  //   });
-
-  //   socket.on("disconnect", () => {
-  //     console.log("A user disconnected");
-  //   });
-  // });
-
-  // IO.use((socket, next) => {
-  //   if (socket.handshake.query) {
-  //     let callerId = socket.handshake.query.callerId;
-  //     socket.user = callerId;
-  //     next();
-  //   }
-  // });
-
-  let onlineUsers = [];
+  const onlineUsers = new Map();
 
   IO.on("connection", (socket) => {
-    console.log(socket.user, "Connected");
-    socket.join(socket.user);
+    socket.join(socket.user.callerId);
 
-    socket.on("userConnect", (userId) => {
-      if (!onlineUsers.some((user) => user === userId)) {
-        // if user is not added before
-        onlineUsers.push(userId);
-        console.log("new user is here!", onlineUsers);
-      }
-      // send all active users to new user
-      socket.emit("onlineUsers", onlineUsers);
+    socket.on("close", (recipientId) => {
+      socket.to(recipientId).emit("disconnectUser", {
+        senderId: socket.user.callerId,
+      });
+    });
 
-      // console.log(userId, "userIduserId");
-      // onlineUsers.add(userId);
-      // socket.emit("onlineUsers", Array.from(onlineUsers));
+    socket.on("login", (userInfo) => {
+      onlineUsers.set(socket.id, userInfo);
+      IO.emit("userConnected", Array.from(onlineUsers.values()));
     });
 
     socket.on("disconnect", () => {
-      onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
-      console.log("user disconnected", onlineUsers);
-      // send all online users to all users
-      socket.emit("onlineUsers", onlineUsers);
+      onlineUsers.delete(socket.id);
+      IO.emit("userDisconnected", Array.from(onlineUsers.values()));
     });
 
     socket.on("call", (data) => {
       let calleeId = data.calleeId;
-      console.log(typeof calleeId);
+
       let rtcMessage = data.rtcMessage;
-      console.log(data, "DATA", calleeId, "sd", socket.user);
+
       socket.to(calleeId).emit("newCall", {
-        callerId: socket.user,
+        callerId: socket.user.callerId,
         rtcMessage: rtcMessage,
       });
     });
@@ -93,30 +49,22 @@ module.exports.initIO = (httpServer) => {
       rtcMessage = data.rtcMessage;
 
       socket.to(callerId).emit("callAnswered", {
-        callee: socket.user,
+        callee: socket.user.callerId,
         rtcMessage: rtcMessage,
       });
     });
 
     socket.on("ICEcandidate", (data) => {
-      console.log("ICEcandidate data.calleeId", data.calleeId);
       let calleeId = data.calleeId;
       let rtcMessage = data.rtcMessage;
 
       socket.to(calleeId).emit("ICEcandidate", {
-        sender: socket.user,
+        sender: socket.user.callerId,
         rtcMessage: rtcMessage,
       });
     });
 
     socket.on("end", (data) => {
-      onlineUsers = onlineUsers.filter((user) => user !== data.calleeId);
-      // onlineUsers.delete(data.calleeId);
-      console.log(onlineUsers, "onlineUsersonlineUsersonlineUsers");
-
-      socket.emit("onlineUsers", onlineUsers);
-
-      // socket.emit("onlineUsers", Array.from(onlineUsers));
       socket.to(data.calleeId).emit("endCall");
     });
   });
@@ -126,7 +74,7 @@ module.exports.getIO = () => {
   if (!IO) {
     throw Error("IO not initilized.");
   } else {
-    console.log("Not initilized");
+    console.log("Initilized");
     return IO;
   }
 };
